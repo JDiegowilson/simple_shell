2a3d66cddd854c6d18166c70e1eb12566f835abc
Ikechukwu Wilson <122816162+JDiegowilson@users.noreply.github.com>
2023-04-22 10:06:18 +0100

    Update helper_p.c

--- helper_p.c
+++ helper_p.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _print - writes a array of chars in the standar output
+ * _print - writes a array of chars in the stdio
  * @string: pointer to the array of chars
  * Return: the number of bytes writed or .
  * On error, -1 is returned, and errno is set appropriately.
